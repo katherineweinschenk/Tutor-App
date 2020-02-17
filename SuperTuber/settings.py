e9880020e77d07862f92697ac64ad62b8afa7415
ccundiff4108 <60150684+ccundiff4108@users.noreply.github.com>
2020-02-17 16:40:39 -0500

    Fixed duplicate app in installed apps

--- SuperTuber/settings.py
+++ SuperTuber/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FindTutors.apps.FindtutorsConfig',
     'crispy_forms'
     'bootstrap4',
 ]
