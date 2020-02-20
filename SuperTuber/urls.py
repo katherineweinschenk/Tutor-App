48c8ccb4d238682e3bf46678492c9b0fd56cf4d4
ccundiff4108 <60150684+ccundiff4108@users.noreply.github.com>
2020-02-20 12:20:22 -0500

    Commented out Tutor and Tutee sign up view
    Html doesn't exist yet for these pages causing master to crash

--- SuperTuber/urls.py
+++ SuperTuber/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.SignUpView.as_view(), name='signup'),
-    path('accounts/signup/tutor/', views.TutorSignUpView.as_view(), name='tutor_signup'),
-    path('accounts/signup/tutee/', views.TuteeSignUpView.as_view(), name='tutee_signup'),
+    #path('accounts/signup/tutor/', views.TutorSignUpView.as_view(), name='tutor_signup'),
+    #path('accounts/signup/tutee/', views.TuteeSignUpView.as_view(), name='tutee_signup'),
 ]
