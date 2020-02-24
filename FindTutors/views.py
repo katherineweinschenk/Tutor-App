3ed5dcecf5502cfdfae84006ede1d23ea56e1fb6
Joe <jps3dc@virginia.edu>
2020-02-24 16:52:45 -0500

    Request now stores the sender

--- FindTutors/views.py
+++ FindTutors/views.py
@@ -4,6 +4,7 @@ from .forms import TutorUserSignUpForm, TuteeUserSignUpForm, RequestForm
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 class HomeView(generic.TemplateView):
     template_name = 'FindTutors/home.html'
@@ -24,9 +25,18 @@ class RequestView(generic.CreateView):
     model = Request
     form_class = RequestForm
     template_name = 'FindTutors/request.html'
+    recipient = None
 
+    def get(self, request, *args, **kwargs):
+        self.recipient = request.GET.get('recipient')
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+    
     def form_valid(self, form):
-        request_input = form.save(commit=False)
+        self.request_input = form.save(commit=False)
+        self.request_input.sender = self.request.user
+        self.request_input.recipient = self.recipient
+        self.request_input.save()
         return redirect('/../home/')        
 
 def signup(request):
