d7f3ebbf6da771942dca8e944ae1b25bdafd3216
Katherine Weinschenk <katherineweinschenk@gmail.com>
2020-02-28 18:10:21 -0500

    static settings

--- SuperTuber/settings.py
+++ SuperTuber/settings.py
@@ -140,7 +140,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 #import django
 django_heroku.settings(locals())
