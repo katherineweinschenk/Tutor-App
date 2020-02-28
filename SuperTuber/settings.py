3bc2a00e8bbbd71559db08c5a953bc71571303e8
Katherine Weinschenk <katherineweinschenk@gmail.com>
2020-02-28 17:31:41 -0500

    delete duplicates

--- SuperTuber/settings.py
+++ SuperTuber/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'FindTutors',
     'social_django',
     'crispy_forms',
     'bootstrap4',
