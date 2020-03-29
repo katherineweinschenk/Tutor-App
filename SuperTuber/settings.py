158fe11b38ebbcccc479e64c06f33b8f95daedaa
julieweiss <48159201+julieweiss@users.noreply.github.com>
2020-03-29 01:03:35 -0400

    Update settings.py

--- SuperTuber/settings.py
+++ SuperTuber/settings.py
@@ -158,8 +158,8 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'FindTutors/static/messages/'),
-    os.path.join(BASE_DIR, 'FindTutors/static/registration/')
-    os.path.join(BASE_DIR, 'FindTutors/static/FindTutors/')
+    os.path.join(BASE_DIR, 'FindTutors/static/registration/'),
+    os.path.join(BASE_DIR, 'FindTutors/static/FindTutors/'),
     os.path.join(BASE_DIR, 'FindTutors/static/SignUp/')
 
 
