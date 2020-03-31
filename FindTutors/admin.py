b655ddefedff01b883f8dfdd2db5755eed2d4acf
ccundiff4108 <60150684+ccundiff4108@users.noreply.github.com>
2020-03-31 18:28:41 -0400

    Added messages to admin page

--- FindTutors/admin.py
+++ FindTutors/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import TUser, Request, Profile
+from .models import TUser, Request, Profile, Room
 #from .models import BigUser
 # admin.site.register(BigUser)
 
