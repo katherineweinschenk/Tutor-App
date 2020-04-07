e86face9478801eb02c2b3b6c0518c6786a623a1
ccundiff4108 <60150684+ccundiff4108@users.noreply.github.com>
2020-04-07 18:29:13 -0400

    Added tests and staticfiles directory

--- FindTutors/tests.py
+++ FindTutors/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.test.client import Client
 from django.db import models
 from FindTutors.models import Profile, TUser, Room
 
@@ -46,25 +47,146 @@ class validUserTests(TestCase):
     def testLastLogin(self):
         self.assertEqual(TUser.objects.get(username="TestUser").last_login, None)
 
-class invalidUserTests(TestCase):
+"""
+class MyProfileTest(TestCase):
     def setUp(self):
-        TUser.objects.create_user("test", "email@email.com")
+        TUser.objects.create_user(username='testUser', email='email@email.com', password='password')
+        self.client = Client()
 
-    def testBadEmail(self):
-        self.assertRaises(TypeError, TUser.objects.create_user, "TestUser")
+    def testMyProfileUsername(self):
+        user = TUser.objects.get(username='testUser')
+        self.client.login(username='testUser', password='password')
+        response = self.client.post('/home/profile/', {'user': user})
 
-    def testBadFirstName(self):
-        self.assertRaises(TypeError, TUser.objects.get(username="test").firstname, 5)
+        self.assertContains(response, 'testUser')
+"""
 
-    def testBadLastName(self):
-        self.assertRaises(TypeError, TUser.objects.get(username="test").lastname, 5)
+class FindTutorsTest(TestCase):
+    def setUp(self):
+        TUser.objects.create_user(username='testUser', email='email@email.com')
 
-    def testBadPhone(self):
-        self.assertRaises(TypeError, TUser.objects.get(username="test").phone_number, "hello")
+    def testRegisterTutorTrue(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = True
+        user.firstname = "TestName"
+        user.save()
 
-    def testBadYear(self):
-        self.assertRaises(TypeError, TUser.objects.get(username="test").year, "hello")
+        response = self.client.get('/home/tutors/')
 
-    def testBadSubjects(self):
-        self.assertRaises(TypeError, TUser.objects.get(username="test").subjects, 5)
+        self.assertContains(response, 'TestName')
 
+    def testRegisterTutorFalse(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = False
+        user.firstname = "TestName"
+        user.save()
+
+        response = self.client.get('/home/tutors/')
+
+        self.assertNotContains(response, 'TestName')
+
+    def testTutorSubjects(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = True
+        user.subjects = 'math'
+        user.save()
+
+        response = self.client.get('/home/tutors/')
+
+        self.assertContains(response, 'math')
+
+    def testTutorYear(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = True
+        user.year = 1
+        user.save()
+
+        response = self.client.get('/home/tutors/')
+
+        self.assertContains(response, 1)
+
+
+#https://www.twilio.com/blog/2018/05/build-chat-python-django-applications-programmable-chat.html
+class MessagingTest(TestCase):
+    def test_all_rooms_are_rendered_in_homepage(self):
+        Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room',
+            validUser1 = 'all',
+            validUser2 = 'all'
+        )
+        Room.objects.create(
+            name='room 2',
+            slug='room-2',
+            description='This is the 2nd room',
+            validUser1 = 'all',
+            validUser2 = 'all'
+        )
+
+        response = self.client.get('/home/messages/')
+
+        self.assertContains(response, 'room 1')
+        self.assertContains(response, 'room 2')
+
+    def test_room_details_are_present_in_room_page(self):
+        room_1 = Room.objects.create(
+            name='room X',
+            slug='room-x',
+            description='This is the X-room',
+            validUser1='all',
+            validUser2='all'
+        )
+
+        response = self.client.get('/home/messages/{}/'.format(room_1.slug))
+
+        self.assertContains(response, room_1.name)
+        self.assertContains(response, room_1.description)
+
+    def test_private_message(self):
+        Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room',
+            validUser1='private',
+            validUser2='private'
+        )
+
+        response = self.client.get('/home/messages/')
+
+        self.assertNotContains(response,'room 1')
+
+    def test_public_message(self):
+        Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room',
+            validUser1='private',
+            validUser2='all'
+        )
+
+        response = self.client.get('/home/messages/')
+
+        self.assertContains(response,'room 1')
+
+    def test_private_and_public(self):
+        Room.objects.create(
+            name='room 1',
+            slug='room-1',
+            description='This is the 1st room',
+            validUser1='private',
+            validUser2='private'
+        )
+
+        Room.objects.create(
+            name='room 2',
+            slug='room-2',
+            description='This is the 2nd room',
+            validUser1='all',
+            validUser2='all'
+        )
+
+        response = self.client.get('/home/messages/')
+
+        self.assertNotContains(response, 'room 1')
+        self.assertContains(response, 'room 2')
