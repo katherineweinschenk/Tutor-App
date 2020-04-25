616b5c621aa251259b7af713eb7518b337305bbc
Joe <jps3dc@virginia.edu>
2020-04-25 12:14:48 -0400

    Update admin.py

--- FindTutors/admin.py
+++ FindTutors/admin.py
@@ -3,7 +3,7 @@ from django_google_maps import widgets as map_widgets
 from django_google_maps import fields as map_fields
 
 # Register your models here.
-from .models import TUser, Request, Profile, Room
+from .models import TUser, Request, Profile, Room, Reviews
 #from .models import BigUser
 # admin.site.register(BigUser)
 
@@ -11,6 +11,7 @@ admin.site.register(TUser)
 admin.site.register(Request)
 admin.site.register(Profile)
 admin.site.register(Room)
+admin.site.register(Reviews)
 
 class RequestAdmin(admin.ModelAdmin):
     formfield_overrides = {
