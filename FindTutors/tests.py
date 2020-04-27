69fc64a7883b4b603dffa1f70c69c5b783f6e93c
ccundiff4108 <60150684+ccundiff4108@users.noreply.github.com>
2020-04-27 15:43:20 -0400

    Added new test cases

--- FindTutors/tests.py
+++ FindTutors/tests.py
@@ -47,19 +47,6 @@ class validUserTests(TestCase):
     def testLastLogin(self):
         self.assertEqual(TUser.objects.get(username="TestUser").last_login, None)
 
-"""
-class MyProfileTest(TestCase):
-    def setUp(self):
-        TUser.objects.create_user(username='testUser', email='email@email.com', password='password')
-        self.client = Client()
-
-    def testMyProfileUsername(self):
-        user = TUser.objects.get(username='testUser')
-        self.client.login(username='testUser', password='password')
-        response = self.client.post('/home/profile/', {'user': user})
-
-        self.assertContains(response, 'testUser')
-"""
 
 class FindTutorsTest(TestCase):
     def setUp(self):
@@ -104,6 +91,65 @@ class FindTutorsTest(TestCase):
         response = self.client.get('/home/tutors/')
 
         self.assertContains(response, 1)
+
+    def testFilter(self):
+        response = self.client.get('/home/tutors/')
+
+        self.assertContains(response, "Filter")
+
+    def testSendRequest(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = True
+        user.save()
+
+        response = self.client.get('/home/tutors/')
+
+        self.assertContains(response, "Send Request")
+
+    def testSendRequestNoTutors(self):
+        response = self.client.get('/home/tutors/')
+
+        self.assertNotContains(response, "Send Request")
+
+
+class ReviewsTest(TestCase):
+    def setUp(self):
+        TUser.objects.create_user(username='testUser', email='email@email.com')
+
+    def testReviews(self):
+        user = TUser.objects.get(username='testUser')
+        user.is_tutor = True
+        user.save()
+
+        response = self.client.get('/home/ratingsreview/')
+
+        self.assertContains(response, "testUser")
+
+    def testReviewsNoTutors(self):
+        response = self.client.get('/home/ratingsreview/')
+
+        self.assertNotContains(response, "testUser")
+
+    def testReviewsRating(self):
+        response = self.client.get('/home/ratingsreview/')
+
+        self.assertContains(response, "Rating:")
+        self.assertContains(response, "Reviews:")
+
+
+class ProfileTest(TestCase):
+    def setUp(self):
+            TUser.objects.create_user(username='testUser', email='email@email.com')
+
+    def testProfilePage(self):
+        response = self.client.get('/home/profile/')
+
+        self.assertContains(response, "Bio")
+
+    def testProfileUpdate(self):
+        response = self.client.get('/home/profile/')
+
+        self.assertContains(response, "Update")
 
 
 #https://www.twilio.com/blog/2018/05/build-chat-python-django-applications-programmable-chat.html
